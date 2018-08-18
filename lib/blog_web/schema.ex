133b1d4c56a91d8f4e6fc850da1319febec91475
Derrick Zhang <derrick.zhang@letote.cn>
2018-08-18 19:32:33 +0800

    Complete resolvers without optmizations

--- lib/blog_web/schema.ex
+++ lib/blog_web/schema.ex
@@ -4,7 +4,16 @@ defmodule BlogWeb.Schema do
   import_types Absinthe.Type.Custom
 
   query do
-    field :posts, list_of(:post)
+    field :posts, list_of(:post) do
+      resolve fn _, _, _ ->
+        {:ok, Blog.list_posts()}
+      end
+    end
+  end
+
+  object :user do
+    field :id, non_null(:id)
+    field :name, non_null(:string)
   end
 
   object :post do
@@ -12,12 +21,42 @@ defmodule BlogWeb.Schema do
     field :title, non_null(:string)
     field :content, non_null(:string)
     field :posted_at, non_null(:datetime)
-    field :comments, list_of(:comment)
+    field :user, non_null(:user) do
+      resolve fn post, _, _ ->
+        query = Ecto.assoc(post, :user)
+        {:ok, Blog.Repo.one(query)}
+      end
+    end
+    field :comments, list_of(:comment) do
+      resolve fn post, _, _ ->
+        query = Ecto.assoc(post, :comments)
+        {:ok, Blog.Repo.all(query)}
+      end
+    end
+    field :comments_count, non_null(:integer) do
+      resolve fn post, _, _ ->
+        import Ecto.Query
+        query = from(c in Blog.Comment, where: c.post_id == ^post.id)
+        {:ok, Blog.Repo.aggregate(query, :count, :id)}
+      end
+    end
   end
 
   object :comment do
     field :id, non_null(:id)
     field :content, non_null(:string)
     field :posted_at, non_null(:datetime)
+    field :post, non_null(:post) do
+      resolve fn comment, _, _ ->
+        query = Ecto.assoc(comment, :post)
+        {:ok, Blog.Repo.one(query)}
+      end
+    end
+    field :user, non_null(:user) do
+      resolve fn comment, _, _ ->
+        query = Ecto.assoc(comment, :user)
+        {:ok, Blog.Repo.one(query)}
+      end
+    end
   end
 end
